88efbd436b07e3744d2b54ff6440f37027a24d1c
Tedikova Olga <tedolga@gmail.com>
2014-08-04 17:50:21 +0400

    Add point class and unit test for it

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,6 +1,86 @@
-/**
- * @author O. Tedikova
- * @version 1.0
- */
-public class Point {
+/*************************************************************************
+ * Name:
+ * Email:
+ *
+ * Compilation:  javac Point.java
+ * Execution:
+ * Dependencies: StdDraw.java
+ *
+ * Description: An immutable data type for points in the plane.
+ *
+ *************************************************************************/
+
+import java.util.Comparator;
+
+public class Point implements Comparable<Point> {
+
+    // compare points by slope
+    public final Comparator<Point> SLOPE_ORDER = new SlopeOrderComparator();       // YOUR DEFINITION HERE
+
+    private int x;                              // x coordinate
+    private int y;                              // y coordinate
+
+    // create the point (x, y)
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+
+    // plot this point to standard drawing
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
+
+    // draw line between this point and that point to standard drawing
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    // slope between this point and that point
+    public double slopeTo(Point that) {
+        if (that.y - this.y == 0 && that.x - this.x == 0) {
+            return Double.NEGATIVE_INFINITY;
+        }
+        if (that.y - this.y == 0) {
+            return 0.0;
+        }
+        if (that.x - this.x == 0) {
+            return Double.POSITIVE_INFINITY;
+        }
+        return (double) (that.y - this.y) / (that.x - this.x);
+    }
+
+    // is this point lexicographically smaller than that one?
+    // comparing y-coordinates and breaking ties by x-coordinates
+    public int compareTo(Point that) {
+        if (this.y < that.y || (this.y == that.y && this.x < that.x)) {
+            return -1;
+        } else {
+            return Integer.valueOf(this.y).compareTo(that.y);
+        }
+    }
+
+    // return string representation of this point
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    // unit test
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
+    }
+
+    private class SlopeOrderComparator implements Comparator<Point> {
+
+        public int compare(Point point1, Point point2) {
+            Point zeroPoint = new Point(x, y);
+            Double slope1 = zeroPoint.slopeTo(point1);
+            Double slope2 = zeroPoint.slopeTo(point2);
+            return slope1.compareTo(slope2);
+        }
+    }
 }
--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -1,4 +1,8 @@
+import java.util.Comparator;
+
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 /**
  * @author O. Tedikova
@@ -7,11 +11,68 @@ import org.junit.Test;
 public class PointTest {
     @Test
     public void testSlopeTo() throws Exception {
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(0, 0);
+        Assert.assertEquals(Double.NEGATIVE_INFINITY, point1.slopeTo(point2));
+        Point point3 = new Point(0, 0);
+        Point point4 = new Point(1, 0);
+        Assert.assertEquals(0.0, point3.slopeTo(point4));
+        Point point5 = new Point(1, 0);
+        Point point6 = new Point(0, 0);
+        Assert.assertEquals(0.0, point5.slopeTo(point6));
+        Point point7 = new Point(0, 1);
+        Point point8 = new Point(0, 0);
+        Assert.assertEquals(Double.POSITIVE_INFINITY, point7.slopeTo(point8));
+        Point point9 = new Point(0, 0);
+        Point point10 = new Point(0, 1);
+        Assert.assertEquals(Double.POSITIVE_INFINITY, point9.slopeTo(point10));
 
+        Point point11 = new Point(0, 0);
+        Point point12 = new Point(1, 1);
+        Assert.assertEquals(1.0, point11.slopeTo(point12));
+
+        Point point13 = new Point(1, 1);
+        Point point14 = new Point(2, 0);
+        Assert.assertEquals(-1.0, point13.slopeTo(point14));
     }
 
     @Test
     public void testCompareTo() throws Exception {
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(0, 0);
+        Assert.assertTrue(point1.compareTo(point2) == 0);
+        Point point3 = new Point(0, 0);
+        Point point4 = new Point(0, 1);
+        Assert.assertTrue(point3.compareTo(point4) < 0);
+        Point point5 = new Point(6, 0);
+        Point point6 = new Point(0, 1);
+        Assert.assertTrue(point5.compareTo(point6) < 0);
+        Point point7 = new Point(0, 1);
+        Point point8 = new Point(1, 1);
+        Assert.assertTrue(point7.compareTo(point8) < 0);
+        Point point9 = new Point(1, 1);
+        Point point10 = new Point(0, 1);
+        Assert.assertTrue(point9.compareTo(point10) == 0);
+        Point point11 = new Point(0, 2);
+        Point point12 = new Point(1, 1);
+        Assert.assertTrue(point11.compareTo(point12) > 0);
+        Point point13 = new Point(1, 2);
+        Point point14 = new Point(0, 1);
+        Assert.assertTrue(point13.compareTo(point14) > 0);
+        Point point15 = new Point(0, 2);
+        Point point16 = new Point(0, 1);
+        Assert.assertTrue(point15.compareTo(point16) > 0);
+    }
+
+    @Test
+    public void testSlopeComparator() throws Exception {
+        Point point1 = new Point(0, 0);
+        Comparator<Point> comparator = point1.SLOPE_ORDER;
+        Point point2 = new Point(2, 0);
+        Point point3 = new Point(3, 0);
+        Assert.assertEquals(0, comparator.compare(point2, point3));
+        Point point4 = new Point(3, -1);
+        Assert.assertEquals(1, comparator.compare(point2, point4));
 
     }
 }
