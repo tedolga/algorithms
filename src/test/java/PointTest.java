b93e1abeb0937783f79e2fa26f2efa273e6763db
Tedikova Olga <tedolga@gmail.com>
2014-08-04 15:19:53 +0400

    Add point class and unit test for it

---
+++ src/test/java/PointTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+/**
+ * @author O. Tedikova
+ * @version 1.0
+ */
+public class PointTest {
+    @Test
+    public void testSlopeTo() throws Exception {
+
+    }
+
+    @Test
+    public void testCompareTo() throws Exception {
+
+    }
+}
